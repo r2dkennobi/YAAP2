ecccd354115bd4a907d2acea1aaf2bd031beb2f6
Kenny Yokoyama <r2dkenny@gmail.com>
2015-11-18 22:45:32 -0800

    Add working receipt filter

--- universal/components/ReceiptList.js
+++ universal/components/ReceiptList.js
@@ -1,4 +1,5 @@
 import React, {PropTypes, Component} from 'react';
+import ReactDOM from 'react-dom';
 import ReceiptItem from './ReceiptItem';
 import { CATEGORIES } from '../constants/ActionTypes';
 
@@ -9,17 +10,41 @@ export default class ReceiptList extends Component {
         actions: PropTypes.object.isRequired
     };
 
+    constructor(props, context) {
+        super(props, context);
+        this.state = { catFilter: '' };
+    }
+
+    handleFilterChange(e) {
+        this.setState({ catFilter: e.target.value });
+    }
+
+    receiptFilter(rcpt) {
+        if (this.state.catFilter === '' || this.state.catFilter === 'All') {
+            return true;
+        } else {
+            return this.state.catFilter === rcpt.category;
+        }
+    }
+
+    componentDidMount() {
+        $(ReactDOM.findDOMNode(this.refs.categoryEl)).material_select();
+    }
+
     render() {
         const { receipts, userId, actions } = this.props;
-        const myReceipts = receipts.filter(receipt => receipt.userId === userId);
+        const myReceipts = receipts.filter(rcpt => rcpt.userId === userId);
         let categories = Object.keys(CATEGORIES).sort();
+        var makeOption = (n, k) => <option value={n} key={k}>{n}</option>;
+        let filteredRcpt = receipts.filter(::this.receiptFilter);
 
         let list;
         let editable = true;
         let totalSum = myReceipts.reduce((x, receipt) => parseFloat(receipt.amount) + x, 0.0);
+        let filteredSum = filteredRcpt.reduce((x, receipt) => parseFloat(receipt.amount) + x, 0.0);
 
-        if (myReceipts.length > 0) {
-            list = myReceipts.map((receipt, key) =>
+        if (filteredRcpt.length > 0) {
+            list = filteredRcpt.map((receipt, key) =>
                 <ReceiptItem key={key}
                              id={receipt.id}
                              editable={editable}
@@ -40,9 +65,20 @@ export default class ReceiptList extends Component {
             <div className='receiptStatus'>
                 <div className='row'>
                     <div className="col s12">
-                        <div className='card blue-grey darken-1'>
+                        <div className='card blue-grey darken-4'>
                             <div className='card-content white-text'>
-                                <span>Total Sum: $ {totalSum}</span>
+                                <span className="card-title">Stats</span>
+                                <p>Total Sum: $ {totalSum}</p>
+                                <p>Filtered Sum: $ {filteredSum}</p>
+                                <hr/>
+                                <span className="card-title">Filter</span>
+                                <select ref="categoryEl"
+                                        className="browser-default black-text"
+                                        value={this.state.category}
+                                        onChange={::this.handleFilterChange}>
+                                    <option value="" disabled>Choose category</option>
+                                    {categories.map(makeOption)}
+                                </select>
                             </div>
                         </div>
                     </div>
